bc465a7bf84eaf4e58b73945ce56c242f79c65af
Muhammad Hidayatur Rahman <hidabaik@gmail.com>
2022-12-01 23:19:53 +0700

    login view

--- lib/module/login/view/login_view.dart
+++ lib/module/login/view/login_view.dart
@@ -1,5 +1,7 @@
+import 'package:fhe_template/widget/form_username.dart';
 import 'package:flutter/material.dart';
 import 'package:fhe_template/core.dart';
+import 'package:hexcolor/hexcolor.dart';
 import '../controller/login_controller.dart';
 
 class LoginView extends StatefulWidget {
@@ -9,12 +11,99 @@ class LoginView extends StatefulWidget {
     controller.view = this;
 
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: const [],
-          ),
+      backgroundColor: HexColor('#FFDE5B'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "images/logo-app.png",
+              width: 260.0,
+              height: 130.0,
+              fit: BoxFit.fill,
+            ),
+            Text(
+              "Selamat Datang",
+              style: TextStyle(
+                shadows: [
+                  Shadow(
+                    offset: const Offset(0.0, 0.0),
+                    blurRadius: 4.0,
+                    color: Colors.black.withOpacity(0.8),
+                  ),
+                ],
+                color: HexColor('#343434'),
+                fontFamily: 'Red Hat Display',
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.5,
+              decoration: BoxDecoration(boxShadow: [
+                BoxShadow(
+                  blurRadius: 8,
+                  offset: const Offset(0.0, 0.0),
+                  color: Colors.black.withOpacity(1.0),
+                  spreadRadius: -9,
+                )
+              ]),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(9.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Column(
+                    children: [
+                      const Text(
+                        "Silahkan login terlebih dahulu",
+                        style: TextStyle(
+                          fontSize: 13.0,
+                          fontFamily: 'Red Hat Display',
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 12.0,
+                      ),
+                      const HDTextFieldForm(hintText: 'Username'),
+                      const SizedBox(
+                        height: 10.0,
+                      ),
+                      const HDTextFieldForm(hintText: 'Password'),
+                      const SizedBox(
+                        width: 10.0,
+                      ),
+                      const SizedBox(
+                        height: 10.0,
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        height: 34.0,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: HexColor('#FFDE59'),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(7.0),
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: const Text(
+                            "LOGIN",
+                            style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 15.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
---
+++ lib/widget/form_username.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class HDTextFieldForm extends StatefulWidget {
+  final String hintText;
+  const HDTextFieldForm({
+    Key? key,
+    required this.hintText,
+  }) : super(key: key);
+
+  @override
+  State<HDTextFieldForm> createState() => _HDTextFieldFormState();
+}
+
+class _HDTextFieldFormState extends State<HDTextFieldForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 40,
+      decoration: BoxDecoration(boxShadow: [
+        BoxShadow(
+          blurRadius: 10,
+          offset: const Offset(0.0, 0.0),
+          color: Colors.black.withOpacity(1.0),
+          spreadRadius: -9,
+        )
+      ]),
+      child: TextFormField(
+        decoration: InputDecoration(
+          fillColor: Colors.white,
+          filled: true,
+          hintText: widget.hintText,
+          hintStyle: const TextStyle(
+            fontFamily: 'Red Hat Display',
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16.0),
+            borderSide: BorderSide(color: HexColor('#FFDE5B'), width: 1.0),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16.0),
+            borderSide: BorderSide(color: HexColor('#C4C4C4'), width: 1.0),
+          ),
+        ),
+        onChanged: (value) {},
+      ),
+    );
+  }
+}
